34ebfae793c5596d7eb92c3b44dd27dad61b691e
myrdiaclonix <myrdiaclonix@proton.me>
2023-08-16 23:48:23 -0300

    chore(homepage): home content done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'currency-converter';
+  title = 'Conversor de Moedas';
+  menuTab = ['Listagem de Moedas', 'Conversão de Moedas', 'Histórico de Conversões'];
 }
